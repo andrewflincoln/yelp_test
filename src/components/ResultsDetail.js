5a52b7183ef71539956110e11bb049ea04341689
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-11-10 18:54:35 -0800

    shows pictures with reviews

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+
+const ResultsDetail = ({ result }) => {
+  return (<View>
+    <Image style={styles.image} source={{ uri: result.image_url }} />
+    <Text style={styles.name}>{result.name}</Text>
+    <Text style={styles.details}>{result.rating} Stars, {result.review_count}</Text>
+  </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  details: {
+    color: 'gray'
+  }
+}) 
+export default ResultsDetail
